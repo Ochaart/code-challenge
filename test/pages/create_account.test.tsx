d16ff77d1488f85de3e039dad91489e714df9618
Ochaart <ochaart@users.noreply.github.com>
2021-07-15 23:58:41 -0700

    updated create_account.test and applied waitFor

--- test/pages/create_account.test.tsx
+++ test/pages/create_account.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import fetchMock from 'jest-fetch-mock';
 import CreateAccount from 'src/pages/create_account';
@@ -12,20 +12,22 @@ describe('CreateAccount', () => {
     fetchMock.resetMocks();
   });
 
-  test('rendering', () => {
-    render(<CreateAccount />);
+  test('rendering', async () => {
+  render(<CreateAccount />)
     fetchMock
-      .once(JSON.stringify({result: false}))
-      .once(JSON.stringify({result: true}))
+      .once(JSON.stringify({ result: false }))
+      .once(JSON.stringify({ result: true }))
     const username = screen.getByLabelText('Username')
     const password = screen.getByLabelText('Password')
     userEvent.type(username, 'michael.perez')
     userEvent.type(password, '!1asdk;lljasdjllkjhaljdlkjlkjad')
     userEvent.click(screen.getByText('Create Account'));
-    expect(fetchMock).toBeCalledTimes(1);
-    expect(fetchMock).toBeCalledWith('/api/password_exposed', {
-      body: JSON.stringify({password: '!1asdk;lljasdjllkjhaljdlkjlkjad'}),
-      method: 'POST',
+    await waitFor(() => {
+      expect(fetchMock).toBeCalledTimes(1);
+      expect(fetchMock).toBeCalledWith('/api/password_exposed', {
+        body: JSON.stringify({ password: '!1asdk;lljasdjllkjhaljdlkjlkjad' }),
+        method: 'POST',
+      })
     })
     // expect(fetchMock).toBeCalledWith('/api/create_new_account', {
     //   body: JSON.stringify({username: "", password: ""}),
